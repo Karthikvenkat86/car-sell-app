f2583c8df89ba7aed0c37e58f12d0c77f961c8ab
Karthikeyan <karthikvenkat86@gmail.com>
2025-08-09 13:32:26 +0530

    Updated big screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,8 +194,14 @@ export default function HomePage() {
        <header className="bg-white shadow-lg">
          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
            <div className="flex justify-between items-center py-4 sm:py-6">
-             <div className="flex items-center w-[60vw]">
-               <Image className="w-full h-auto" src="/green-carz-logo.svg" alt="green Carz" width={800} height={320} />
+             <div className="flex items-center w-[60vw] sm:w-auto">
+               <Image
+                 className="w-full h-auto sm:w-auto sm:h-16 md:h-20"
+                 src="/green-carz-logo.svg"
+                 alt="green Carz"
+                 width={800}
+                 height={320}
+               />
              </div>
              <div className="flex flex-col items-end">
                <div className="flex items-center space-x-2">
